c3ad398653ee3bf19bfedaddb43ab23b41255ff6
Sorin Chis <kish.sorin@yahoo.com>
2024-07-05 15:07:36 +0200

    add components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,27 @@
+import HeaderBox from '../../components/HeaderBox';
+import RightSidebar from '../../components/RightSideBar';
+import TotalBalanceBox from '../../components/TotalBalanceBox';
+
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: 'Adrian' };
+  return (
+    <div className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions"
+          />
+
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1223.5} />
+
+          <RightSidebar user={loggedIn} />
+        </header>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
